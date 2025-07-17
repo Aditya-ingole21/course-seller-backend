cf8c8322e343fb5848e92e3a35fb06713f7f949e
Aditya-ingole21 <aditayingole113@gmail.com>
2025-07-17 22:37:48 +0530

    added admin routes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const  {userRoutes}= require ("./routes/user");
 const  {courseRoutes}= require ("./routes/course");
+const {adminRoutes} = require ("./routes/admin");
 
 app.use("/user",userRoutes);
 app.use("/course",courseRoutes);
+app.use("/admin",adminRoutes);
 
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const {Router} = require("express");
+
+const adminRoutes = Router();
+
+adminRoutes.post("/signup",function(req,res) {
+
+});
+adminRoutes.post("/signin",function(req,res) {
+
+});
+
+
+module.exports ={
+adminRoutes:adminRoutes
+}
