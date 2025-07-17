968a6b16228468162b64cc11d9c423ad8ae9376f
Aditya-ingole21 <aditayingole113@gmail.com>
2025-07-17 22:45:37 +0530

    modified admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,18 @@ adminRoutes.post("/signup",function(req,res) {
 adminRoutes.post("/signin",function(req,res) {
 
 });
+adminRoutes.post("/course",function(req,res) {
+
+});
+adminRoutes.put("/course",function(req,res) {
+
+});
+adminRoutes.get("/course/bulk",function(req,res) {
+
+});
+
+
+
 
 
 module.exports ={
